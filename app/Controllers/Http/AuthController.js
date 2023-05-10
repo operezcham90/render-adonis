6c7fb3239c30421b2ba2f328387ee3e9fff41c0b
Oscar E. Perez-Cham <39531018+operezcham90@users.noreply.github.com>
2023-05-10 17:16:06 -0600

    🔐

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class AuthController {
+    showRegisterForm({ view }) {
+        return view.render('register')
+    }
+    showLoginForm() {
+        return view.render('login')
+    }
+    async register({ request, response }) {
+        const userData = request.only(['username', 'email', 'password'])
+        const user = await User.create(userData)
+        return response.json(user)
+    }
+    async login({ request, response, auth }) {
+        const { email, password } = request.all()
+        const token = await auth.attempt(email, password)
+        return response.json(token)
+    }
+}
+
+module.exports = AuthController
