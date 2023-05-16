5a887da544eb2659a77808d6906fa969c56f6bbc
Oscar E. Perez-Cham <39531018+operezcham90@users.noreply.github.com>
2023-05-16 17:10:06 -0600

    🐙

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,6 +1,24 @@
+const { Octokit } = require("@octokit/rest")
+const fs = require('fs')
+
 class HomeController {
     index({ view }) {
         return view.render('welcome')
+    }
+
+    async upload({ response }) {
+        const octokit = new Octokit({
+            auth: process.env.GITHUB_TOKEN || ''
+        })
+        const buff = fs.readFileSync('mensaje')
+        const file = await octokit.repos.createOrUpdateFileContents({
+            owner: 'operezcham90',
+            repo: 'prueba-octokit',
+            path: 'mensaje',
+            message: 'upload',
+            content: buff.toString('base64')
+        })
+        return response.json(file)
     }
 }
 
