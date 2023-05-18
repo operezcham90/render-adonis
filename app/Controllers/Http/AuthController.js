c2d9e954ea2ebc49b47ab9346810e44bad5d1117
Oscar E. Perez-Cham <39531018+operezcham90@users.noreply.github.com>
2023-05-18 15:29:36 -0600

    👌

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const User = use('App/Models/User')
+const { validate } = use('Validator')
 
 class AuthController {
     showRegisterForm({ view }) {
@@ -9,7 +10,29 @@ class AuthController {
     showLoginForm({ view }) {
         return view.render('login')
     }
-    async register({ request, response }) {
+    async register({ request, session, response }) {
+        const rules = {
+            username: 'required|unique:users,username',
+            email: 'required|email|unique:users,email',
+            password: 'required'
+        }
+        const messages = {
+            'username.required': 'El campo nombre de usuario es obligatorio.',
+            'email.required': 'El campo correo electrónico es obligatorio.',
+            'username.unique': 'El nombre de usuario ya está ocupado.',
+            'email.unique': 'El correo electrónico ya está ocupado.',
+            'email.email': 'El campo correo electrónico debe ser una dirección de correo válida.',
+            'password.required': 'El campo contraseña es obligatorio.'
+        }
+        const validation = await validate(request.all(), rules, messages)
+        if (validation.fails()) {
+            // Lógica en caso de validación fallida
+            session
+                .withErrors(validation.messages())
+                .flashExcept(['password'])
+            return response.redirect('back')
+        }
+
         const userData = request.only(['username', 'email', 'password'])
         await User.create(userData)
         return response.redirect('/login')
